36b7793bb1a5dbf7e931b657deac6df09a468e25
yudhawan <yudhaashter@gmail.com>
2024-08-08 21:14:07 +0700

    product added

--- api/createProduct.go
+++ api/createProduct.go
@@ -4,10 +4,19 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
+	"umikyoRest/libs"
 )
 
 type CreateProductType struct {
-	product_name string
+	ProductName string    `json:"product_name" bson:"product_name"`
+	Quantity    int64     `json:"quantity" bson:"quantity"`
+	Price       int64     `json:"price" bson:"price"`
+	Category    string    `json:"category" bson:"category"`
+	SubCategory *string   `json:"sub,omitempty" bson:"sub,omitempty"`
+	Images      *[]string `json:"images,omitempty" bson:"images,omitempty"`
+	Description *string   `json:"desc,omitempty" bson:"desc,omitempty"`
+	CreatedDate time.Time `json:"created_date" bson:"created_date"`
 }
 
 func createProduct(w http.ResponseWriter, r *http.Request) {
@@ -17,12 +26,16 @@ func createProduct(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		fmt.Print(err)
 	}
-	// obj := libs.Field{FieldName: "Testing"}
-	fmt.Println(t)
-	// res, err := obj.Insert(t)
-	// if err != nil {
-	// 	libs.SendErrorResponse(w, "Failed to add Data", 405)
-	// 	return
-	// }
-	json.NewEncoder(w).Encode("res.InsertedID")
+	// to print out as an object
+	tJson, _ := json.Marshal(t)
+	fmt.Println(string(tJson))
+	//
+	t.CreatedDate = time.Now()
+	obj := libs.Field{FieldName: "Testing"}
+	res, err := obj.Insert(t)
+	if err != nil {
+		libs.SendErrorResponse(w, "Failed to add Data", 405)
+		return
+	}
+	json.NewEncoder(w).Encode(res)
 }
